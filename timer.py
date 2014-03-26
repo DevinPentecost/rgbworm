99e11d0d245345f363c0a8b3e3a6414b2b808c4c
dpentecost <devinpentecost@gmail.com>
2014-03-26 10:02:22 -0700

    Added some libraries for time/memory optimization. Theres a lot of work to do to speed this up.

---
+++ timer.py
@@ -0,0 +1,24 @@
+"""
+Code Timer
+Python class for assisting with timing particular portions of executed code.
+Original Source Code can be found here:
+http://www.huyng.com/posts/python-performance-analysis/
+"""
+
+
+import time
+
+class Timer(object):
+    def __init__(self, verbose=False):
+        self.verbose = verbose
+
+    def __enter__(self):
+        self.start = time.time()
+        return self
+
+    def __exit__(self, *args):
+        self.end = time.time()
+        self.secs = self.end - self.start
+        self.msecs = self.secs * 1000  # millisecs
+        if self.verbose:
+            print 'elapsed time: %f ms' % self.msecs
